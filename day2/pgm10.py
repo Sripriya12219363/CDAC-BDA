cab02dbb763063be10549d45fc3be3e69718130a
Sripriya12219363 <119854017+Sripriya12219363@users.noreply.github.com>
2026-09-12 15:48:02 +0530

    Refactor main function to count consecutive characters
    Refactored the main function to count consecutive characters in a string and print the result. Removed commented-out code related to inventory.

--- day2/pgm10.py
+++ day2/pgm10.py
@@ -1,29 +1,19 @@
-# def main():
-#     s=input("Enter the string: ")
-#     l=s.split(" ")
-    
-#     res=""
-#     for i in range(0,len(l)):
-#         num=0
-#         for j in range(0, len(l)):
-#             if(l[i]==l[j]):
-#                 num+=1
-#         res+=l[i]+str(num)
-#     print(res)
-# main()
+def main():
+    s = input("Enter the string: ")
+    res = ""
+    count = 1
+
+    for i in range(0, len(s)):
+        if i + 1 < len(s) and s[i] == s[i + 1]:
+            count += 1
+        else:
+            res += s[i] + str(count)
+            count = 1
+
+    if len(res) < len(s):
+        print(res)
+    else:
+        print(s)
 
 
-inventory = {"apples": 10, "bananas": 24}
-
-# View of keys
-keys_view = inventory.keys()
-print("Keys:", list(keys_view))  # Output: ['apples', 'bananas']
-
-
-# View of values
-values_view = inventory.values()
-print("Values:", list(values_view))  # Output: [10, 24]
-
-# View of key-value tuples
-items_view = inventory.items()
-print("Items:", list(items_view))  # Output: [('apples', 10), ('bananas', 24)]
+main()
